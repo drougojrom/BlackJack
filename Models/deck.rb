1b237d11197a2f7f9e8307d814d16f58ae653a87
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-01 13:53:33 +0200

    deck - added

---
+++ Models/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+ def initialize
+   @cards = []
+   add_pack
+ end
+
+ def add_pack
+   deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+   suits = ["H", "D", "C", "S"]
+   shuffled_deck = (deck.product(suits) * pack_count).shuffle
+   shuffled_deck.each do |cards|
+     @cards.push Card.new(cards[0], cards[1], true)
+   end
+ end
+
+ def give_card(player, show=true)
+   card = @cards.pop
+   card.show if show
+   player.hand.push card
+ end
+end
