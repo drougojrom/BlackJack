6998785ff56758155edd118107ab9d54b11565b5
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-02 14:47:03 +0200

    dealer - added

--- Models/dealer.rb
+++ Models/dealer.rb
@@ -1,10 +1,11 @@
 class Dealer
-  attr_accessor :name, :hand, :tally
+  attr_accessor :name, :hand, :tally, :bank
 
   def initalize(name)
     @name = name
     @hand = []
     @tally = 0
+    @bank = 100
   end
 
   def show_hand
@@ -12,5 +13,25 @@ class Dealer
     @hand.each do |card|
       puts card.show
     end
+  end
+
+  def calculate_total
+    total = 0
+    ace_count = 0
+    @hand.each do |card|
+      total += card.value
+      ace_count = += 1 if card.value == 11
+    end
+    ace_count.each do |card|
+      total -= 10 if total > Game::BLACKJACK
+    end
+  end
+
+  def blackjack?
+    self.calculate_total == Game::BLACKJACK
+  end
+
+  def lost?
+    self.calculate_total > Game::BLACKJACK
   end
 end
