c82592d696473bac032d8f21fa5e6b4c7b7b06c5
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-04 23:25:21 +0200

    refactoring WIP

--- game.rb
+++ game.rb
@@ -14,34 +14,24 @@ class Game
   def initialize(player, dealer, deck)
     @player = player
     @dealer = dealer
-    @deck = deck
-    @player_total = 0
-    @dealer_total = 0
   end
 
   def play
-    result = game_result
-    @dealer.bank -= result
-    @player.bank += result
-
     loop do
-      if @player.bank > 0 && @dealer.bank > 0
-        break unless restarted_game = GameInterface.start_game?(@player, @dealer)
-        @dealer = restarted_game[:dealer]
-        @player = restarted_game[:player]
-        @deck = restarted_game[:deck]
-        @player_total = restarted_game[:player_total]
-        @dealer_total = restarted_game[:dealer_total]
-        result = game_result
-        @dealer.bank -= result
-        @player.bank += result
-      else
-        break
-      end
+      break if @player.bank == 0 || @dealer.bank == 0
+      break unless restarted_game = GameInterface.start_game?(@player, @dealer)
+      @dealer.hand = []
+      @player.hand = []
+      @deck = Deck.new
+      @player_total = 0
+      @dealer_total = 0
+      result = game_result
+      @dealer.bank -= result
+      @player.bank += result
     end
   end
 
-  private
+private
 
   def game_result
     2.times do
@@ -49,44 +39,45 @@ class Game
       self.deck.give_card(@dealer, false)
     end
 
-    player_total = player.calculate_total
-    dealer_total = dealer.calculate_total
+    @player_total = player.calculate_total
+    @dealer_total = dealer.calculate_total
 
-    @player.display_hand
+    loop do
+      take_skip = GameInterface.first_turn
+      handle_player_turn(take_skip)
+    end
 
-    take_skip = GameInterface.first_turn
-
-    handle_player_turn(take_skip)
-
-    check_for_blackjack
+    if blackjack = check_for_blackjack
+      handle_result(@player.name, blackjack)
+    end
 
     handle_dealer_turn
 
-    GameInterface.display_cards(@player, @dealer)
+    handle_end_game
+  end
 
-    if player_total > dealer_total
-      GameInterface.display_result(@player.name, true)
-      return 10
+  def handle_end_game
+    if @player_total > @dealer_total
+      return handle_result(@player.name, true)
     elsif dealer_total > player_total
-      GameInterface.display_result(@player.name, false)
-      return -10
+      return handle_result(@player.name, false)
     else
       GameInterface.display_result(@player.name, nil)
     end
   end
 
+  def handle_result(name, win)
+    GameInterface.display_result(name, win)
+    return win ? 10 : -10
+  end
+
   def check_for_blackjack
     if @player.blackjack?
-      @player.display_hand
-      @dealer.display_hand
-      GameInterface.display_result(@player.name, true)
-      return 10
-    end
-    if @dealer.blackjack?
-      @player.display_hand
-      @dealer.dispay_hand
-      GameInterface.display_result(@player.name, false)
-      return -10
+      return true
+    elsif @dealer.blackjack?
+      return false
+    else
+      return nil
     end
   end
 
@@ -96,12 +87,11 @@ class Game
       @player_total = @player.calculate_total
       if @player.lost?
         GameInterface.display_cards(@player, @dealer)
-        GameInterface.display_result(@player.name, false)
-        return -10
+        return handle_result(@player.name, false)
       end
-    elsif take_skip != 's'
-      GameInterface.show_error
-      return 0
+    elsif take_skip == 's'
+      GameInterface.display_cards(@player)
+    elsif take_skip == 'p'
     end
   end
 
@@ -111,8 +101,7 @@ class Game
       @dealer_total = @dealer.calculate_total
       if dealer_total > BLACKJACK
         GameInterface.display_cards(@player, @dealer)
-        GameInterface.display_result(@player.name, true)
-        return 10
+        return handle_result(@player.name, true)
       end
     end
   end
--- game_interface.rb
+++ game_interface.rb
@@ -1,40 +1,34 @@
 class GameInterface
 
   def self.start_game?(player, dealer)
-    puts 'Do you want to play? y/n'
-    continue = gets.chomp
     puts "#{player.name} has #{player.bank}$"
     puts "Dealer has #{dealer.bank}$"
-    case continue
-    when 'y'
-      deck = Deck.new
-      player.hand = []
-      dealer.hand = []
-      player_total = 0
-      dealer_total = 0
-      {dealer: dealer,
-       player: player,
-       deck: deck,
-       player_total: player_total,
-       dealer_total: dealer_total}
-    when 'n'
-      return nil
+    puts 'Do you want to play? Y - 1; N - 2'
+    choice = gets.to_i
+    case choice
+    when 1
+      true
+    when 2
+      false
     else
       puts 'not a valid value'
     end
   end
 
   def self.first_turn
-    puts 'Take another card or skip? T/S'
+    puts 'Take another card, pass or show hand? T/P/S'
     take_skip = gets.chomp.downcase
+    take_skip
   end
 
-  def self.display_cards(player, dealer)
+  def self.display_cards(player, dealer = nil)
     puts 'Your cards: '
     player.display_hand
     puts ''
-    puts 'My cards: '
-    dealer.display_hand
+    if dealer
+      puts 'My cards: '
+      dealer.display_hand
+    end
   end
 
   def self.display_result(player_name, result)
